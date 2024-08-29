1c54cb42dd03922f403de449c3388ed5b73bcc25
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2024-08-29 00:52:15 -0500

    Repositorio

--- Evaluacion/PC1/RANDY TERRY CENTENO LEIVA/PRACT01/REQ04.sql
+++
@@ -1,5 +0,0 @@
-select (E.firstname+E.lastname) [Nombre empleado], count(1) [Cantidad de pedidos], sum(od.quantity) [Cantidad de productos], sum(OD.UnitPrice * OD.Quantity) [Importe de las ventas]
-, sum(OD.UnitPrice * OD.Quantity) / (select sum(OD.UnitPrice * OD.Quantity) from [Order Details] OD) * 100 [Porcentaje del total] from orders O
-join [order details] OD on OD.orderid = O.orderid
-join employees E on E.employeeid = O.employeeid
-group by (E.firstname+E.lastname)
